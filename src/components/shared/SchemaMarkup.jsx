476ca78862d70af90cd7c65d09e9e430240579af
mosh-d <shopsy8@gmail.com>
2026-01-14 15:31:25 +0100

    Implementing schema generation

--- src/components/shared/SchemaMarkup.jsx
+++ src/components/shared/SchemaMarkup.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Helmet } from "react-helmet-async";
+import { Helmet } from "react-helmet";
 
 /**
  * Schema Component - Fetches and injects JSON-LD schema markup
